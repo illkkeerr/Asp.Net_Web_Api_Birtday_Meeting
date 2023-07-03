0818e5c42e49598274e804a35277b82731c1a01c
illkkeerr <ilkersenel5797@gmail.com>
2023-07-03 20:13:34 +0300

    Asp.Net_Web_Api_Birtday_Meeting

---
+++ BirtdayApp/BirtdayApp/Models/DavetiyeModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BirtdayApp.Models
+{
+    public class DavetiyeModel
+    {
+        [Required]
+        public string Ad { get; set; }
+        [Required]
+        public string EPosta { get; set; }
+        [Required]
+        public bool? KatilmaDurumu { get; set; }
+    }
+}
